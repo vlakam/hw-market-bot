4d6358c8b49f94214f5003cbb1c88746f74fe0eb
Ainenya <ainenya@MacBook-Pro-Ainenya.local>
2020-09-07 16:51:47 +0300

    forward message

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const app = require('http').createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('App init');
 });
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +10,5 @@ app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
-console.log('before market bot');
 const hwMarketBot = require('./src/app.js');
 hwMarketBot.start();
--- src/app.js
+++ src/app.js
@@ -4,10 +4,43 @@ const Markup = require('telegraf/markup')
 exports.start = (config) => {
     const bot = new Telegraf(process.env.BOT_TOKEN)
     bot.start((ctx) => {
-        console.log('welcome')
-        ctx.reply('Welcome!')
+        ctx.reply('Describe lot')
     })
-    bot.help((ctx) => ctx.reply('Send me a sticker'))
+    bot.help((ctx) => ctx.reply('Help'))
     bot.on('sticker', (ctx) => ctx.reply('👍'))
+    bot.on('text', (ctx) => {
+        const message = ctx.message;
+        const lotInfo = message.text;
+        const user = {
+            id: message.from.id,
+            name: message.from.first_name,
+            surname: message.from.last_name,
+            getMentionById: function() {
+                return `#id${this.id}`
+            },
+            getMentionByFullNameMarkdown: function() {
+                //[inline mention of a user](tg://user?id=123456789)
+                return `[${this.name} ${this.surname}](tg://user?id=${this.id})`
+            },
+            getMentionByFullNameHtml: function() {
+                return `<a href="tg://user?id=${this.id}">${this.getFullName()}</a>[${this.getMentionById()}]`
+            },
+            getFullName: function() {
+                return `${this.name} ${this.surname}`
+            }
+        };
+        console.log(JSON.stringify(message))
+
+        const formattedMessage = `
+            ${user.getMentionByFullNameHtml()}\n
+            Лот: ${lotInfo}
+        `
+        const extraParams = {
+            parse_mode: 'HTML'
+        }
+
+        ctx.telegram.sendMessage(process.env.MODERATION_CHAT_ID, formattedMessage, extraParams)
+      })
+      
     bot.launch()
 }
